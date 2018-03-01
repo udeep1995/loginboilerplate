92c35585ae22676807931a8c01161417989f513b
Udeep <udeep@nagarro.com>
2018-03-01 12:52:51 +0530

    Phone number Authentication

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const $ = require('jquery');
+const $ = require('jquery/dist/jquery.min');
 const firebase = require('firebase');
  $(document).ready(function () {
    function initFirebase () {
@@ -8,6 +8,9 @@ const firebase = require('firebase');
        success: handler
      })
    }
+   function handler (config) {
+     firebase.initializeApp(config);
+}
   $('#signinButton').click(function() {
     var provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('email');
@@ -19,8 +22,41 @@ const firebase = require('firebase');
       console.log(user);
   });
 });
-  function handler (config) {
-    firebase.initializeApp(config);
+
+$('#auth_phn_btn').click(function(e) {
+  e.preventDefault();
+  firebase.auth().useDeviceLanguage();
+  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('auth_phn_btn', {
+    'size': 'invisible',
+    'callback': function(response) {
+      onSignInSubmit();
+    }
+  });
+  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+  var appVerifier = window.recaptchaVerifier;
+  var phone = $('#PhoneNumber').val();
+  if (phone) {
+    firebase.auth().signInWithPhoneNumber(phone, appVerifier)
+    .then(function (confirmationResult) {
+      console.log(confirmationResult);
+      window.confirmationResult = confirmationResult;
+      $('#auth_code').click((e)=> {
+        e.preventDefault();
+        var code = $('#Code').val();
+          confirmationResult.confirm(code).then(function (result) {
+          var user = result.user;
+          console.log(user);
+        }).catch(function (error) {
+          console.log('Code error ', error);
+        });
+      });
+    } , (err) => {
+      window.recaptchaVerifier.render().then((widgetId) => {
+        grecaptcha.reset(widgetId);
+      });
+        console.log('Not worked ', err);
+    });
   }
+});
   initFirebase();
 });
--- readme.md
+++ readme.md
@@ -2,10 +2,10 @@
   This is basic login boiler-plate code using firebase and nodejs
 
 ## SETUP
-  1 Clone the project
-  2 Create a new firebase project.
-  3 Create new directory in cloned project and rename it as 'config'
-  4 Add new file with name 'development.env'
-  5 Add the config variables in newly created file
-  6 Change sign in method in firebase and enable google and add localhost as domain name
-  7 Start the project using `npm start`
+  1 Clone the project__
+  2 Create a new firebase project__
+  3 Create new directory in cloned project and rename it as 'config'__
+  4 Add new file with name 'development.env'__
+  5 Add the config variables in newly created file__
+  6 Change sign in method in firebase and enable google and add localhost as domain name__
+  7 Start the project using `npm start`__
